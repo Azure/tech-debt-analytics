79f292d848e2efedc5969444aad60c2a1552ef5a
Steph Locke <stephlocke@microsoft.com>
2024-05-09 11:10:57 +0100

    Change input data methodology

--- shipper/shipper.cs
+++ shipper/shipper.cs
@@ -1,23 +1,22 @@
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
 using System.Web;
 using System.Threading.Tasks;
 using System.IO;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
 namespace appcatdl
 {
     public static class Shipper
     {
-
         [Function("shipper")]
         [BlobOutput("appcat/{org}/{repo}/{branch}/{pr}/{commit}/{committer}/{DateTime.Now:yyyyMMddHHmmss}.json", Connection = "AzureWebJobsStorage")]
-        public static async Task<string> Ship([HttpTrigger(AuthorizationLevel.Anonymous, "put")] HttpRequest req,
-            ILogger log)
+        public static async Task<string> Ship([HttpTrigger(AuthorizationLevel.Anonymous, "put")] HttpRequestData req,
+            ILogger log,
+            FunctionContext executionContext)
         {
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var query = HttpUtility.ParseQueryString(req.QueryString.Value);
+            string requestBody = await req.ReadAsStringAsync();
+            var query = HttpUtility.ParseQueryString(req.Url.Query);
             var org = query["org"] ?? "00";
             var repo = query["repo"] ?? "00";
             var branch = query["branch"] ?? "00";
